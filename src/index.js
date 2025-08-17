1bc2b71f6ee7ff8320509763409d17e09dcab24b
satyam1298 <lakshyajee03@gmail.com>
2025-08-17 09:22:47 +0530

    database connect ho gaya

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+// import express from "express"
+// // function connectDB(){}
+// // connectDB
+// ;  (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>
+//         {
+//             console.log("error:",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log("App is listening on port")
+//         })
+
+//     }catch(error){
+//         console.error("ERROR:",error);
+//         throw err
+//     }
+
+// })()
